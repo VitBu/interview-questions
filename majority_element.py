4823cca45f5b459ec954d3b0716eee838ae7a08b
VitBu <33810987+VitBu@users.noreply.github.com>
2022-10-18 21:03:58 +0300

    Update majority_element.py
    1. Fix for the case that there was an empty list in the input.
    2. Added tests.

--- majority_element.py
+++ majority_element.py
@@ -1,12 +1,25 @@
 # Given an array of size n find the element that appears more than floor(n/2) times, if it doesn't exist return None.
 
+
 from collections import Counter
 
 
-def majorityElement(self, A):
+def majority_element(A):
     counter = Counter(A)
-    most_common, times = counter.most_common(1)[0]
+    most_common_result = counter.most_common(1)
+    if most_common_result == []:
+        return None
+
+    most_common, times = most_common_result[0]
     if times > len(A) // 2:
         return most_common
     
     return None
+
+
+
+if __name__ == '__main__':
+    assert majority_element([]) == None
+    assert majority_element([1]) == 1
+    assert majority_element([1, 2]) == None
+    assert majority_element([1, 3, 1]) == 1
